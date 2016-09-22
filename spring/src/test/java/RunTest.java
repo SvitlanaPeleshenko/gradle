1913af62f95502334b7d7b62af39cc7a06259d0b
Svitlana Peleshenko <Svitlana_Peleshenko@epam.com>
2016-09-22 18:59:21 +0300

    Add files

--- spring/src/test/java/RunTest.java
+++ spring/src/test/java/RunTest.java
@@ -4,10 +4,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
-/*@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(format = {"pretty", "html:build/reports","pretty:build/cucumber-report.json"})
-public class RunTest {}*/
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="D:\\SOAP_BDD2\\spring\\src\\test\\resources\\features\\add.feature")
 public class RunTest {}
+
+/*@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="D:\\SOAP_BDD2\\spring\\src\\test\\resources\\features\\add.feature")
+public class RunTest {}*/
